97ea644587f3804bfa1a5df7f3252e12ad0948f2
Ashraf Ali Musalman <ashraf.ali@cleanstart.com>
2026-03-23 17:37:44 +0530

    added teh prometheus setup

--- app/main.py
+++ app/main.py
@@ -1,13 +1,14 @@
-# app/main.py
 from fastapi import FastAPI
 from app.database import engine, Base
 
-# import models so SQLAlchemy registers them
 from app.models.doctor_model import Doctor
 from app.models.hospital_model import Hospital
 
 from app.routers import auth_router, patient_router
 from app.routers.admin import doctor_router
+
+# 👇 ADD THIS
+from prometheus_fastapi_instrumentator import Instrumentator
 
 app = FastAPI(title="Hospital API")
 
@@ -16,3 +17,6 @@ Base.metadata.create_all(bind=engine)
 app.include_router(auth_router.router)
 app.include_router(patient_router.router)
 app.include_router(doctor_router.router)
+
+# 👇 ADD THIS (VERY IMPORTANT)
+Instrumentator().instrument(app).expose(app)
