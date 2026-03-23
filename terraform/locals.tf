ba1c2d1d7feaf510c8ed83a1ab445e77bb87b6df
Ashraf Ali Musalman <ashraf.ali@cleanstart.com>
2026-03-23 16:32:40 +0530

    added few changes

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -2,6 +2,6 @@ locals {
   aws_access_key = data.vault_kv_secret_v2.aws.data["access_key"]
   aws_secret_key = data.vault_kv_secret_v2.aws.data["secret_key"]
 
-  db_username = data.vault_kv_secret_v2.db.data["db_username"]
-  db_password = data.vault_kv_secret_v2.db.data["db_password"]
+  db_username = data.vault_kv_secret_v2.db.data["username"]
+  db_password = data.vault_kv_secret_v2.db.data["password"]
 }
--- terraform/vault.tf
+++ terraform/vault.tf
@@ -4,7 +4,7 @@ provider "vault" {
     auth_login {
         path = "auth/approle/login"
         parameters = {
-            role_id=" e1983a2a-6c8d-6992-7788-bd750cf4e465"
+            role_id="e1983a2a-6c8d-6992-7788-bd750cf4e465"
             secret_id="24518e97-5394-3362-8b08-52389655f387"
         }
       
