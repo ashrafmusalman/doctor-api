977c1eebb5d48cddab1c1de87aad01b5f3b2d4ee
Ashraf ali musalman <ashraf.musalman.ali@gmail.com>
2026-03-23 15:26:20 +0530

    Change Vault provider address and auth credentials
    Updated Vault provider address and authentication parameters.

--- terraform/vault.tf
+++ terraform/vault.tf
@@ -1,11 +1,11 @@
 provider "vault" {
-    address = "http://100.53.135.0:8200"
+    address = "http://18.207.125.97:8200/"
 
     auth_login {
         path = "auth/approle/login"
         parameters = {
-            role_id="455ca385-38dd-ee10-832e-ef5137e79bc7"
-            secret_id="70822243-5df0-59ea-aaaf-4044749ea04b"
+            role_id=" e1983a2a-6c8d-6992-7788-bd750cf4e465"
+            secret_id="24518e97-5394-3362-8b08-52389655f387"
         }
       
     }
